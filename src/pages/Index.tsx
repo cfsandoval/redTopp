812d4519dfccceae883be830b1bb45bf566303ed
cfsandoval <cfsandoval@gmail.com>
2025-09-13 02:02:03 -0300

    [dyad] Fixed undefined config error by adding default configuration and robust type handling in NetworkGraph component - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,24 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
 import NetworkGraph from "@/components/NetworkGraph";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Node, Link, NetworkSimulationConfig } from "@/types/network";
+
+const sampleNodes: Node[] = [
+  { id: '1', name: 'Server 1', x: 100, y: 100, type: 'server' },
+  { id: '2', name: 'Workstation 1', x: 200, y: 200, type: 'workstation' },
+  { id: '3', name: 'Router 1', x: 300, y: 150, type: 'router' }
+];
+
+const sampleLinks: Link[] = [
+  { source: '1', target: '2' },
+  { source: '2', target: '3' }
+];
+
+const sampleConfig: NetworkSimulationConfig = {
+  width: 800,
+  height: 400,
+  nodeRadius: 15
+};
 
 const Index = () => {
   return (
@@ -12,7 +30,11 @@ const Index = () => {
           <CardTitle>Network Topology Simulator</CardTitle>
         </CardHeader>
         <CardContent>
-          <NetworkGraph />
+          <NetworkGraph 
+            nodes={sampleNodes} 
+            links={sampleLinks} 
+            config={sampleConfig} 
+          />
         </CardContent>
       </Card>
       <MadeWithDyad />
